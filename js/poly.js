41af6f01fe9f28718afac0911062f26171638ceb
Aaron <aahill@vassar.edu>
2015-09-16 17:25:12 -0400

    added gradient base to canvas

--- js/poly.js
+++ js/poly.js
@@ -1,8 +1,18 @@
+var MAX_STOPS = 4;
+var MIN_STOPS = 2;
+
+var numStops = Math.floor(MAX_STOPS + (1+MIN_STOPS-MAX_STOPS)*Math.random());
 var canvas = document.getElementById("polyCanvas");
 var ctx = canvas.getContext("2d");
-
 var gradient = ctx.createLinearGradient(0,0,170,0);
-gradient.addColorStop(0, "black");
-gradient.addColorStop(1, "white");
+
+function generateColor(){
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+for(var i=0; i<MAX_STOPS; i++){
+    gradient.addColorStop(Math.random(), generateColor());
+}
+
 ctx.fillStyle=gradient;
-ctx.fillRect(20,20,150,100);
+ctx.fillRect(0,0,300,300);
