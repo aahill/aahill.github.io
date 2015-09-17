c27a81da419513cbc96e8dad97722211a0ab073c
Aaron <aahill@vassar.edu>
2015-09-16 22:46:28 -0400

    working poly project

--- js/poly.js
+++ js/poly.js
@@ -16,6 +16,12 @@ function generateWholeNum(min, max){
 	return Math.floor((Math.random() * max) + min);
 }
 
+function getTriangleCenter(p1,p2,p3){
+	var centerX = (p1[0],p2[0],p3[0])/3;
+    var centerY = (p1[1],p2[1],p3[1])/3;
+    return [centerX, centerY];
+}
+
 //generate a random hex color
 function generateColor(){
     var color ='#'+Math.floor(Math.random()*16777215).toString(16);
@@ -23,17 +29,29 @@ function generateColor(){
     return color;
 }
 
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+function getPixel(x, y){
+    var p = ctx.getImageData(x, y, 1, 1).data; 
+    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);  
+    return hex;
+}
+
 for(var i=0; i<numStops; i++){
     gradient.addColorStop(Math.random(), generateColor());
 }
 
 ctx.fillStyle=gradient;
-ctx.fillRect(0,0,300,300);
+ctx.fillRect(0,0,500,500);
 
 //randomly place points on the canvas
 for(var i=0; i< numTrianglePoints; i++){
-	var x = generateWholeNum(0,300);
-	var y = generateWholeNum(0,300);
+	var x = generateWholeNum(0,500);
+	var y = generateWholeNum(0,500);
     trianglePoints.push([x,y]);
 	ctx.rect(x,y,1,1);
     ctx.fillStyle="black";
@@ -47,8 +65,9 @@ for(var i=0; i < triangleVertices.length - 1 - triangleVertices.length % 3; i +=
     var point1 = trianglePoints[triangleVertices[i]];
     var point2 = trianglePoints[triangleVertices[i+1]];
     var point3 = trianglePoints[triangleVertices[i+2]];
-    console.log(i);
-    //line 1
+
+    //line 1  
+    ctx.strokeStyle = "rgba(255,255,255,1)";
     ctx.beginPath();
     ctx.moveTo(point1[0], point1[1]);
     ctx.lineTo(point2[0],point2[1]);
@@ -57,4 +76,10 @@ for(var i=0; i < triangleVertices.length - 1 - triangleVertices.length % 3; i +=
     //line 3
     ctx.lineTo(point1[0],point1[1]);
     ctx.stroke();
+    var triangleCenter = getTriangleCenter(point1,point2,point3);
+    var color = getPixel(triangleCenter[0], triangleCenter[1]);
+    console.log('resulting hex: ', color);
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.fill();
 }
