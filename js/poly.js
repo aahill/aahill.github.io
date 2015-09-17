db730d25c4a9528e9cc8bf033a1f2786d7b735fa
Aaron <aahill@vassar.edu>
2015-09-16 21:00:18 -0400

    iniitial implementation of delauney triangulation

--- js/poly.js
+++ js/poly.js
@@ -4,8 +4,7 @@ var MIN_STOPS = 2;
 //number of actual stops
 var numStops = generateWholeNum(MIN_STOPS, MAX_STOPS);
 //number of triangle points
-var numTrianglePoints = generateWholeNum(20,40);
-
+var numTrianglePoints = generateWholeNum(15,33);
 
 var trianglePoints = [];
 
@@ -14,7 +13,7 @@ var ctx = canvas.getContext("2d");
 var gradient = ctx.createLinearGradient(0,0,170,0);
 
 function generateWholeNum(min, max){
-	Math.floor((Math.random() * max) + min);
+	return Math.floor((Math.random() * max) + min);
 }
 
 //generate a random hex color
@@ -24,7 +23,7 @@ function generateColor(){
     return color;
 }
 
-for(var i=0; i<MAX_STOPS; i++){
+for(var i=0; i<numStops; i++){
     gradient.addColorStop(Math.random(), generateColor());
 }
 
@@ -35,5 +34,33 @@ ctx.fillRect(0,0,300,300);
 for(var i=0; i< numTrianglePoints; i++){
 	var x = generateWholeNum(0,300);
 	var y = generateWholeNum(0,300);
-	ctx.fillRect(x,y,5,5);
+    trianglePoints.push([x,y]);
+	ctx.rect(x,y,1,1);
+    ctx.fillStyle="black";
+	ctx.fill();
+}
+
+//begin triangulation process
+var triangleVertices = Delaunay.triangulate(trianglePoints);
+
+for(var i=0; i < triangleVertices.length - triangleVertices.length % 3; i += 2){
+    var vertRef = triangleVertices[i];
+    var point1 = trianglePoints[vertRef];
+    var point2 = trianglePoints[vertRef+1];
+    var point3 = trianglePoints[vertRef+2];
+    //line 1
+    ctx.beginPath();
+    ctx.moveTo(point1[0], point1[1]);
+    ctx.lineTo(point2[0],point2[1]);
+    ctx.stroke();
+    //line 2
+    ctx.beginPath();
+    ctx.moveTo(point2[0], point2[1]);
+    ctx.lineTo(point3[0],point3[1]);
+    ctx.stroke();
+    //line 3
+    ctx.beginPath();
+    ctx.moveTo(point3[0], point3[1]);
+    ctx.lineTo(point1[0],point1[1]);
+    ctx.stroke();
 }
