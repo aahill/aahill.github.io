22afe5d81a1eb0564a31d8007f998b3214646482
Aaron <aahill@vassar.edu>
2015-09-29 14:59:11 -0400

    major site restructure

--- js/controllers/contentController.js
+++ js/controllers/contentController.js
@@ -1,7 +1,11 @@
 app.controller("contentController", ['$scope', function($scope){
 	$scope.displayContent = true;
+    $scope.content="welcome";
 	$scope.test = "thanks";
 	$scope.al = function(){
 		alert('hi');
 	};
+    $scope.switchContent = function(contentName){
+        $scope.content = contentName;
+    }
 }]);
